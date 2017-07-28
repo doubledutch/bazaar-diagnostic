0b3c7cbd79005201e46794360ee23c9540d35053
Nicholas Clark <nbclark@gmail.com>
2017-07-27 17:41:28 -0700

    Add bazaar diagnostic tool

---
+++ mobile/index.web.js
@@ -0,0 +1,20 @@
+var React = require('react-native');
+var { AppRegistry } = React;
+import HomeView from './src/home-view.js'
+
+console.error = () => { }
+console.disableYellowBox = true;
+
+const runApp = (DD) => {
+  AppRegistry.registerComponent('bazaar_diagnostic', () => HomeView);
+  AppRegistry.runApplication('bazaar_diagnostic', {
+    rootTag: document.getElementById('react-root'),
+    initialProps: { ddOverride: DD }
+  })
+}
+
+if (window.DD && window.DD.Events) {
+  Bazaar.WebShim.install((DD) => runApp)
+} else {
+  runApp(null)
+}
